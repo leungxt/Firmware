f769b90b1ddfd96c3df22420ee8e6d81dd905442
leungxt <51778432+leungxt@users.noreply.github.com>
2021-01-12 13:51:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Add a feed source
-sed -i '$a src-git https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
