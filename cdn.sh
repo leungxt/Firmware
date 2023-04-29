3a09a945e32a06c0e7538ab08394929554557ac3
leungxt <51778432+leungxt@users.noreply.github.com>
2023-04-30 07:30:57 +0800

    Create cdn.sh

---
+++ cdn.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+cp -r /tmp/upload/passwall /etc/config/
+
+cp -r /tmp/upload/adbyby /etc/config/
+
+wget -O /root/cf.sh https://raw.githubusercontent.com/leungxt/OpenWrt-Firmware/main/cf.sh
+
+wget -O /root/ip.txt https://raw.githubusercontent.com/leungxt/OpenWrt-Firmware/main/ip.txt
+
+wget -O /root/CloudflareST https://github.com/leungxt/OpenWrt-Firmware/raw/main/CloudflareST
+
+chmod +x /root/cf.sh  &&  chmod +x /root/CloudflareSTroot
+
+# 30 4 * * * /root/cf.sh > /dev/null 
